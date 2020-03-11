b39c1e2bf7c50e86aade185c5df29419dc7936f3
David Halma <31989319+amaraa44@users.noreply.github.com>
2020-03-11 21:17:53 +0100

    ValidatorBuilder

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -17,32 +17,20 @@ class MainTest {
     @Test
     void test_validate_integer_doesNotThrowException() {
         assertDoesNotThrow(() ->
-                new Validator()
-                        .validatableObject(1)
-                        .validatableObject(4)
-                        .validatableObject(3, 4, 6, 8, 12)
-                        .rule(new ObjectNotNullRule(), new GreaterThanZeroRule())
-                        .exception(new IllegalArgumentException(EXCEPTION_MESSAGE))
-                        .validate());
-    }
-
-    @Test
-    void test_validate_integer_doesNotThrowException1() {
-        assertDoesNotThrow(() ->
-                new Validator()
+                new Validator.builder()
                         .validatableObject(1)
                         .validatableObject(4)
                         .validatableObject(3, 4, 6, 8, 12)
                         .rule(new ObjectNotNullRule())
                         .rule(new GreaterThanZeroRule())
                         .exception(new IllegalArgumentException(EXCEPTION_MESSAGE))
-                        .validate());
+                        .build().validate());
     }
 
     @Test
     void test_validate_integer_throwsException() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
-                new Validator()
+                new Validator.builder()
                         .validatableObject(1)
                         .validatableObject(4)
                         .validatableObject(3, 4, 6, 8, 0)
@@ -50,8 +38,7 @@ class MainTest {
                         .rule(new ObjectNotNullRule()) 
                         //TODO: Handle duplicates
                         .exception(new IllegalArgumentException(EXCEPTION_MESSAGE))
-                        .validate()
-        );
+                        .build().validate());
         assertEquals(EXCEPTION_MESSAGE, exception.getMessage());
     }
     
@@ -59,62 +46,58 @@ class MainTest {
     void test_validate_integer_throwsException1() {
         List<Integer> integers = Arrays.asList(432, 54, 877, 12355, -1);
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
-                new Validator()
+                new Validator.builder()
                         .validatableObject(1)
                         .validatableObject(4)
                         .validatableObject(integers)
                         .rule(new ObjectNotNullRule())
                         .rule(new GreaterThanZeroRule())
                         .exception(new IllegalArgumentException(EXCEPTION_MESSAGE))
-                        .validate()
-        );
+                        .build().validate());
         assertEquals(EXCEPTION_MESSAGE, exception.getMessage());
     }    
     
     @Test
     void test_validate_object_throwsException() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
-                new Validator()
+                new Validator.builder()
                         .validatableObject("", null)
                         .rule(new NotEmptyStringRule())
                         .exception(new IllegalArgumentException(EXCEPTION_MESSAGE))
-                        .validate()
-        );
+                        .build().validate());
         assertEquals(EXCEPTION_MESSAGE, exception.getMessage());
     }
 
     @Test
     void test_validate_object_doesNotThrowException() {
         assertDoesNotThrow(() ->
-                new Validator()
+                new Validator.builder()
                         .validatableObject("asdf", " fjdsl")
                         .rule(new NotEmptyStringRule())
                         .exception(new IllegalArgumentException(EXCEPTION_MESSAGE))
-                        .validate());
+                        .build().validate());
     }
 
     @Test
     void test_validate_object_throwsException1() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
-                new Validator()
+                new Validator.builder()
                         .validatableObject("123")
                         .rule(new NotEmptyStringRule())
                         .rule(new GreaterThanZeroRule())
                         .exception(new IllegalArgumentException(EXCEPTION_MESSAGE))
-                        .validate()
-        );
+                        .build().validate());
         assertEquals(EXCEPTION_MESSAGE, exception.getMessage());
     }
 
     @Test
     void test_validate_object_throwsException2() {
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () ->
-                new Validator()
+                new Validator.builder()
                         .validatableObject("asd", null)
                         .rule(new NotEmptyStringRule())
                         .exception(new IllegalArgumentException(EXCEPTION_MESSAGE))
-                        .validate()
-        );
+                        .build().validate());
         assertEquals(EXCEPTION_MESSAGE, exception.getMessage());
     }
 }
