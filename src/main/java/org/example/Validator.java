d74df56700d7adfd01919263c45cb58c8ab3c01e
David Halma <31989319+amaraa44@users.noreply.github.com>
2020-03-11 21:18:47 +0100

    ValidatorBuilder

--- src/main/java/org/example/Validator.java
+++ src/main/java/org/example/Validator.java
@@ -1,51 +1,22 @@
 package org.example;
 
-import lombok.Data;
 import org.example.rule.Rule;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-@Data
 public class Validator {
-
-    private List<Object> validatableObjects = new ArrayList<>();
-    private List<Rule> rules = new ArrayList<>();
+    private List<Object> validatableObjects;
+    private List<Rule> rules;
     private RuntimeException exception;
 
-    public Validator validatableObject(Object object){
-        validatableObjects.add(object);
-        return this;
+    public List<Object> getValidatableObjects() {
+        return validatableObjects;
     }
 
-    public Validator validatableObject(List<Object> validatableObjects){
-        this.validatableObjects.addAll(validatableObjects);
-        return this;
-    }
-
-    public Validator validatableObject(Object... validatableObjects){
-        this.validatableObjects.addAll(Arrays.asList(validatableObjects));
-        return this;
-    }
-
-    public Validator exception(RuntimeException exception){
-        this.exception = exception;
-        return this;
-    }
-    public Validator rule(Rule rule){
-        this.rules.add(rule);
-        return this;
-    }
-
-    public Validator rule(List<Rule> rules){
-        this.rules.addAll(rules);
-        return this;
-    }
-
-    public Validator rule(Rule... rules){
-        this.rules.addAll(Arrays.asList(rules));
-        return this;
+    public void setValidatableObjects(List<Object> validatableObjects) {
+        this.validatableObjects = validatableObjects;
     }
 
     public void validate() {
@@ -59,9 +30,73 @@ public class Validator {
     }
 
     private void throwIfNotValid(Object o, Rule rule) {
-        if (!rule.isValid(o)){
+        if (!rule.isValid(o)) {
             throw exception;
         }
     }
 
+    public List<Rule> getRules() {
+        return rules;
+    }
+
+    public void setRules(List<Rule> rules) {
+        this.rules = rules;
+    }
+
+    public RuntimeException getException() {
+        return exception;
+    }
+
+    public void setException(RuntimeException exception) {
+        this.exception = exception;
+    }
+
+    public static class builder {
+        private List<Object> validatableObjects = new ArrayList<>();
+        private List<Rule> rules = new ArrayList<>();
+        private RuntimeException exception;
+
+        public builder validatableObject(Object object) {
+            validatableObjects.add(object);
+            return this;
+        }
+
+        public builder validatableObject(List<Object> validatableObjects) {
+            this.validatableObjects.addAll(validatableObjects);
+            return this;
+        }
+
+        public builder validatableObject(Object... validatableObjects) {
+            this.validatableObjects.addAll(Arrays.asList(validatableObjects));
+            return this;
+        }
+
+        public builder exception(RuntimeException exception) {
+            this.exception = exception;
+            return this;
+        }
+
+        public builder rule(Rule rule) {
+            this.rules.add(rule);
+            return this;
+        }
+
+        public builder rule(List<Rule> rules) {
+            this.rules.addAll(rules);
+            return this;
+        }
+
+        public builder rule(Rule... rules) {
+            this.rules.addAll(Arrays.asList(rules));
+            return this;
+        }
+
+        public Validator build() {
+            Validator validator = new Validator();
+            validator.setValidatableObjects(validatableObjects);
+            validator.setRules(rules);
+            validator.setException(exception);
+            return validator;
+        }
+    }
 }
